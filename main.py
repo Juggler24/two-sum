b92c32046ee67c2726fbc94d185dbbfc58349744
Juggler24 <eat8934@naver.com>
2023-10-04 11:21:48 +0000

    main.py changed

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
-import re
-#nums = [2,7,11,15], target = 9
+from typing import List
 
-num_list = []
-target = 0
-user_input = input("Input: ")
-user_input = user_input.split("[")[1]
-flag = False
-
-i = 0
-temp = user_input.split(",")
-while i < len(temp):
-    num_list.append(int(re.sub(r'[^0-9]', '', temp[i])))
-    i += 1
-target = num_list[len(num_list)-1]
-num_list.pop()
-
-for i in range(len(num_list)-1):
-    for j in range(i+1, len(num_list)):
-        if num_list[i] + num_list[j] == target:
-            flag = True
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                flag = True
+            if flag == True:
+                break
         if flag == True:
             break
-    if flag == True:
-        break
+    
+    temp = []
+    temp.append(i)
+    temp.append(j)
+    return temp
+    
 
-print("Output: ["+str(i) + "," + str(j)+"]")
+#nums = [2,7,11,15]
+#target = 9
